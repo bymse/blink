2349907cb2516e87b67a43571679f5def57f3ddd
bymse <egorstrelov@gmail.com>
2023-09-12 12:02:10 +0300

    Api client impl

--- src/client-app/app/connect/apiClient.ts
+++ src/client-app/app/connect/apiClient.ts
@@ -11,21 +11,39 @@ interface ICreateResponse extends ITokenResponse {
 interface IApiClient {
     create(): Promise<ICreateResponse>
 
-    activate(connection_id: string): Promise<ITokenResponse>
+    activate(connection_id: string): Promise<ITokenResponse>,
+    
+    submit(token: string, url: string): Promise<void>
 }
 
 const client: IApiClient = {
-    async activate(connection_id: string): Promise<ITokenResponse> {
+    activate(connection_id: string): Promise<ITokenResponse> {
         return send<ITokenResponse>(`/api/connect/activate?connection_id=${connection_id}`, 'POST');
     },
     create(): Promise<ICreateResponse> {
         return send<ICreateResponse>('/api/connect/create', 'POST');
+    },
+    submit(token: string, url: string): Promise<void> {
+        return send<void>('api/connect/submit', 'POST', token, {url});
     }
 }
 
-async function send<T>(url: string, method: "GET" | "POST") {
+async function send<T>(url: string, method: "GET" | "POST", token?: string, body?: any) {
     const absoluteUrl = new URL(url, config.apiHost);
-    const response = await fetch(absoluteUrl, {method});
+    const headers = new Headers();
+    const init: RequestInit = {
+        method,
+    }
+    if (token) {
+        headers.set('Authorization', `Bearer ${token}`);
+    }
+    if (body) {
+        init.body = JSON.stringify(body);
+        headers.set('Content-Type', 'application/json');
+    }
+    init.headers = headers;
+    const response = await fetch(absoluteUrl, init);
+    
     return await response.json() as T;
 }
 
