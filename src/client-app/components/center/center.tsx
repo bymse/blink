5bbaf96783e898e8527641c9199b771cb977154d
bymse <egorstrelov@gmail.com>
2023-11-11 14:10:43 +0300

    Items alignment fixes

--- src/client-app/components/center/center.tsx
+++ src/client-app/components/center/center.tsx
@@ -1,11 +1,14 @@
 import React from "react";
 import styles from "./center.module.scss"
+
 export default function Center({
-  children,
-}: {
-  children: React.ReactNode
-}) { 
+                                   children,
+                               }: {
+    children: React.ReactNode
+}) {
     return <section className={styles.center}>
-        {children}
+        <div className={styles.items}>
+            {children}
+        </div>
     </section>
 }
